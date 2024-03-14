c9ec89f61372eb1c45e08e1357ae36492e4eebdc
darli <darli@DESKTOP-C55CHJN>
2024-03-13 22:24:09 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Paises
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbl_Continente = new Label();
+            lbl_Informacion = new Label();
+            lbl_Religion = new Label();
+            lbl_Moneda = new Label();
+            lbl_Gobierno = new Label();
+            lbl_Poblacion = new Label();
+            lbl_Idioma = new Label();
+            lbl_Capital = new Label();
+            lbl_Presidente = new Label();
+            txt_Gobierno = new TextBox();
+            txt_Poblacion = new TextBox();
+            txt_Moneda = new TextBox();
+            txt_Religion = new TextBox();
+            txt_Idioma = new TextBox();
+            txt_Continente = new TextBox();
+            txt_Capital = new TextBox();
+            txt_Presidente = new TextBox();
+            label1 = new Label();
+            btn_Salir = new Button();
+            Cbo_Paises = new ComboBox();
+            btn_Limpiar = new Button();
+            SuspendLayout();
+            // 
+            // lbl_Continente
+            // 
+            lbl_Continente.AutoSize = true;
+            lbl_Continente.Font = new Font("Microsoft New Tai Lue", 12F, FontStyle.Bold);
+            lbl_Continente.Location = new Point(281, 103);
+            lbl_Continente.Name = "lbl_Continente";
+            lbl_Continente.Size = new Size(95, 21);
+            lbl_Continente.TabIndex = 40;
+            lbl_Continente.Text = "Continente";
+            // 
+            // lbl_Informacion
+            // 
+            lbl_Informacion.AutoSize = true;
+            lbl_Informacion.Font = new Font("Microsoft New Tai Lue", 12F, FontStyle.Bold);
+            lbl_Informacion.Location = new Point(451, 26);
+            lbl_Informacion.Name = "lbl_Informacion";
+            lbl_Informacion.Size = new Size(111, 21);
+            lbl_Informacion.TabIndex = 39;
+            lbl_Informacion.Text = "DESARROLLO";
+            // 
+            // lbl_Religion
+            // 
+            lbl_Religion.AutoSize = true;
+            lbl_Religion.Font = new Font("Microsoft New Tai Lue", 12F, FontStyle.Bold);
+            lbl_Religion.Location = new Point(281, 338);
+            lbl_Religion.Name = "lbl_Religion";
+            lbl_Religion.Size = new Size(74, 21);
+            lbl_Religion.TabIndex = 38;
+            lbl_Religion.Text = "Religion";
+            // 
+            // lbl_Moneda
+            // 
+            lbl_Moneda.AutoSize = true;
+            lbl_Moneda.Font = new Font("Microsoft New Tai Lue", 12F, FontStyle.Bold);
+            lbl_Moneda.Location = new Point(281, 294);
+            lbl_Moneda.Name = "lbl_Moneda";
+            lbl_Moneda.Size = new Size(73, 21);
+            lbl_Moneda.TabIndex = 37;
+            lbl_Moneda.Text = "Moneda";
+            // 
+            // lbl_Gobierno
+            // 
+            lbl_Gobierno.AutoSize = true;
+            lbl_Gobierno.Font = new Font("Microsoft New Tai Lue", 12F, FontStyle.Bold);
+            lbl_Gobierno.Location = new Point(281, 261);
+            lbl_Gobierno.Name = "lbl_Gobierno";
+            lbl_Gobierno.Size = new Size(81, 21);
+            lbl_Gobierno.TabIndex = 36;
+            lbl_Gobierno.Text = "Gobierno";
+            // 
+            // lbl_Poblacion
+            // 
+            lbl_Poblacion.AutoSize = true;
+            lbl_Poblacion.Font = new Font("Microsoft New Tai Lue", 12F, FontStyle.Bold);
+            lbl_Poblacion.Location = new Point(281, 223);
+            lbl_Poblacion.Name = "lbl_Poblacion";
+            lbl_Poblacion.Size = new Size(87, 21);
+            lbl_Poblacion.TabIndex = 35;
+            lbl_Poblacion.Text = "Poblacion";
+            // 
+            // lbl_Idioma
+            // 
+            lbl_Idioma.AutoSize = true;
+            lbl_Idioma.Font = new Font("Microsoft New Tai Lue", 12F, FontStyle.Bold);
+            lbl_Idioma.Location = new Point(281, 181);
+            lbl_Idioma.Name = "lbl_Idioma";
+            lbl_Idioma.Size = new Size(64, 21);
+            lbl_Idioma.TabIndex = 34;
+            lbl_Idioma.Text = "Idioma";
+            // 
+            // lbl_Capital
+            // 
+            lbl_Capital.AutoSize = true;
+            lbl_Capital.Font = new Font("Microsoft New Tai Lue", 12F, FontStyle.Bold);
+            lbl_Capital.Location = new Point(281, 140);
+            lbl_Capital.Name = "lbl_Capital";
+            lbl_Capital.Size = new Size(64, 21);
+            lbl_Capital.TabIndex = 33;
+            lbl_Capital.Text = "Capital";
+            // 
+            // lbl_Presidente
+            // 
+            lbl_Presidente.AutoSize = true;
+            lbl_Presidente.Font = new Font("Microsoft New Tai Lue", 12F, FontStyle.Bold);
+            lbl_Presidente.Location = new Point(281, 72);
+            lbl_Presidente.Name = "lbl_Presidente";
+            lbl_Presidente.Size = new Size(91, 21);
+            lbl_Presidente.TabIndex = 32;
+            lbl_Presidente.Text = "Presidente";
+            // 
+            // txt_Gobierno
+            // 
+            txt_Gobierno.Location = new Point(389, 259);
+            txt_Gobierno.Name = "txt_Gobierno";
+            txt_Gobierno.Size = new Size(277, 23);
+            txt_Gobierno.TabIndex = 31;
+            // 
+            // txt_Poblacion
+            // 
+            txt_Poblacion.Location = new Point(389, 221);
+            txt_Poblacion.Name = "txt_Poblacion";
+            txt_Poblacion.Size = new Size(277, 23);
+            txt_Poblacion.TabIndex = 30;
+            // 
+            // txt_Moneda
+            // 
+            txt_Moneda.Location = new Point(389, 297);
+            txt_Moneda.Name = "txt_Moneda";
+            txt_Moneda.Size = new Size(277, 23);
+            txt_Moneda.TabIndex = 29;
+            // 
+            // txt_Religion
+            // 
+            txt_Religion.Location = new Point(389, 336);
+            txt_Religion.Name = "txt_Religion";
+            txt_Religion.Size = new Size(277, 23);
+            txt_Religion.TabIndex = 28;
+            // 
+            // txt_Idioma
+            // 
+            txt_Idioma.Location = new Point(389, 179);
+            txt_Idioma.Name = "txt_Idioma";
+            txt_Idioma.Size = new Size(277, 23);
+            txt_Idioma.TabIndex = 27;
+            // 
+            // txt_Continente
+            // 
+            txt_Continente.Location = new Point(389, 109);
+            txt_Continente.Name = "txt_Continente";
+            txt_Continente.Size = new Size(277, 23);
+            txt_Continente.TabIndex = 26;
+            // 
+            // txt_Capital
+            // 
+            txt_Capital.Location = new Point(389, 138);
+            txt_Capital.Name = "txt_Capital";
+            txt_Capital.Size = new Size(277, 23);
+            txt_Capital.TabIndex = 25;
+            // 
+            // txt_Presidente
+            // 
+            txt_Presidente.Location = new Point(389, 70);
+            txt_Presidente.Name = "txt_Presidente";
+            txt_Presidente.Size = new Size(277, 23);
+            txt_Presidente.TabIndex = 24;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft New Tai Lue", 12F, FontStyle.Bold);
+            label1.Location = new Point(151, 111);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 21);
+            label1.TabIndex = 23;
+            label1.Text = "Paises";
+            // 
+            // btn_Salir
+            // 
+            btn_Salir.BackColor = SystemColors.Window;
+            btn_Salir.Font = new Font("Microsoft New Tai Lue", 12F, FontStyle.Bold);
+            btn_Salir.Location = new Point(0, 11);
+            btn_Salir.Name = "btn_Salir";
+            btn_Salir.Size = new Size(104, 36);
+            btn_Salir.TabIndex = 22;
+            btn_Salir.Text = "Salir";
+            btn_Salir.UseVisualStyleBackColor = false;
+            btn_Salir.Click += btn_Salir_Click;
+            // 
+            // Cbo_Paises
+            // 
+            Cbo_Paises.FormattingEnabled = true;
+            Cbo_Paises.Items.AddRange(new object[] { "Mexico", "Argentina", "Estados Unidos", "Panama", "Republica Dominicana", "Francia", "Haiti", "Canada", "Venezuela", "Ecuador" });
+            Cbo_Paises.Location = new Point(65, 140);
+            Cbo_Paises.Name = "Cbo_Paises";
+            Cbo_Paises.Size = new Size(173, 23);
+            Cbo_Paises.TabIndex = 21;
+            Cbo_Paises.SelectedIndexChanged += Cbo_Paises_SelectedIndexChanged;
+            // 
+            // btn_Limpiar
+            // 
+            btn_Limpiar.BackColor = SystemColors.Window;
+            btn_Limpiar.Font = new Font("Microsoft New Tai Lue", 12F, FontStyle.Bold);
+            btn_Limpiar.Location = new Point(104, 208);
+            btn_Limpiar.Name = "btn_Limpiar";
+            btn_Limpiar.Size = new Size(104, 36);
+            btn_Limpiar.TabIndex = 41;
+            btn_Limpiar.Text = "Limpiar";
+            btn_Limpiar.UseVisualStyleBackColor = false;
+            btn_Limpiar.Click += btn_Limpiar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(747, 375);
+            Controls.Add(btn_Limpiar);
+            Controls.Add(lbl_Continente);
+            Controls.Add(lbl_Informacion);
+            Controls.Add(lbl_Religion);
+            Controls.Add(lbl_Moneda);
+            Controls.Add(lbl_Gobierno);
+            Controls.Add(lbl_Poblacion);
+            Controls.Add(lbl_Idioma);
+            Controls.Add(lbl_Capital);
+            Controls.Add(lbl_Presidente);
+            Controls.Add(txt_Gobierno);
+            Controls.Add(txt_Poblacion);
+            Controls.Add(txt_Moneda);
+            Controls.Add(txt_Religion);
+            Controls.Add(txt_Idioma);
+            Controls.Add(txt_Continente);
+            Controls.Add(txt_Capital);
+            Controls.Add(txt_Presidente);
+            Controls.Add(label1);
+            Controls.Add(btn_Salir);
+            Controls.Add(Cbo_Paises);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbl_Continente;
+        private Label lbl_Informacion;
+        private Label lbl_Religion;
+        private Label lbl_Moneda;
+        private Label lbl_Gobierno;
+        private Label lbl_Poblacion;
+        private Label lbl_Idioma;
+        private Label lbl_Capital;
+        private Label lbl_Presidente;
+        private TextBox txt_Gobierno;
+        private TextBox txt_Poblacion;
+        private TextBox txt_Moneda;
+        private TextBox txt_Religion;
+        private TextBox txt_Idioma;
+        private TextBox txt_Continente;
+        private TextBox txt_Capital;
+        private TextBox txt_Presidente;
+        private Label label1;
+        private Button btn_Salir;
+        private ComboBox Cbo_Paises;
+        private Button btn_Limpiar;
+    }
+}
